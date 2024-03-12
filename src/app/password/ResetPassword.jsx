6c046eb41cc7cfcb38b18b89fc6ab021905c3da7
Abdullah-Haytham <hedeya@gmail.com>
2024-03-12 23:55:11 +0200

    Added a from info components that displays info above certain forms

--- src/app/password/ResetPassword.jsx
+++ src/app/password/ResetPassword.jsx
@@ -1,20 +1,38 @@
-import React from "react";
-import Image from "next/image";
-import logo from "../assets/logoSpreadIt.svg"
+import React, {useState} from "react";
+import FormInfo from "../components/form/FormInfo.jsx";
 
 function ResetPassword() {
+    const [formData, setFormData] = useState({username: "", email: ""})
+
+    function handleInputChange(event) {
+        const {name, value} = event.target;
+        setFormData(prevFormData => ({...prevFormData, [name]: value}))
+    }
     return (
-      <div className="PageColumn__left"> 
-        <div className="form_logo">
-            <Image src={logo} height={40}/>
+      <div className="pageColumn__right">
+        <div className="userFormContainer"> 
+            <FormInfo 
+            title="Reset your password" 
+            description="Tell us the username and email address associated with your Reddit account, and we'll send you an email with a link to reset your password."
+            />
+            <form>
+                <div>
+                    <input name="username" 
+                        type="text"
+                        placeholder="Username"
+                        onChange={handleInputChange} 
+                    />
+                </div>
+
+                <div>
+                <input name="email" 
+                    type="text" 
+                    placeholder="Email" 
+                    onChange={handleInputChange}
+                />
+                </div>
+            </form>
         </div>
-        <h1 className="Title">Reset your password</h1>
-        <p className="Description">
-            Tell us the username and email address associated with your Reddit account, and we'll send you an email with a link to reset your password.
-        </p>
-        <form className="PasswordForm">
-            
-        </form>
       </div>
     );
   }
