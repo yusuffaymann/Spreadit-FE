ea5a1730f9b00aebd05abe9b62102375598c1230
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2024-03-19 00:27:08 +0200

    Modularized code and shortened the Profile_images component

--- src/app/settings/profile/Profile_images.jsx
+++ src/app/settings/profile/Profile_images.jsx
@@ -1,47 +1,25 @@
 import Image from "next/image";
 import React from "react";
 import "./Profile.css";
+import AvatarArea from "./AvatarArea";
+import BannerArea from "./BannerArea"
 
-export default function ProfileImages()
-{
-    return (
-        <div >
-            <div className="settings--flex">
+export default function ProfileImages() {
+  return (
+    <div>
+      <div className="settings--flex">
         <div className="settings--flexheader">
           <h3>Avatar and banner image</h3>
-          <p>
-            Images must be .png or .jpg format
-          </p>
+          <p>Images must be .png or .jpg format</p>
         </div>
         <div class="settings--flexoption">
-          <div class="profile--images-flexdrag">
-            <div class="profile--avatar">
-              <label class="profile--images-dragarea  profile--images-border">
-                <img src="Add.png" class="profile--images-icon"></img>
-                <div class="profile--images-text">
-                  <span >
-                    Drag and Drop or Upload{" "}
-                    <span class="profile--images-textbold">Avatar</span> Image
-                  </span>
-                </div>
-                <div class="acceptinput"></div>
-              </label>
-            </div>
-            <div class="profile--banner">
-              <label class="profile--images-dragarea  profile--images-border">
-                <img src="Add.png" class="profile--images-icon"></img>
-                <div class="profile--images-text">
-                  <span >
-                    Drag and Drop or Upload{" "}
-                    <span class="profile--images-textbold">Banner</span> Image
-                  </span>
-                </div>
-                <div class="acceptinput"></div>
-              </label>
-            </div>
+          <div className="profile--images-flexdrag">
+            <AvatarArea/>
+            <BannerArea/>
           </div>
         </div>
       </div>
-        </div>
-    );
+      
+    </div>
+  );
 }
