92ad22c438451c111ebc56ac4d72631fe7b371b8
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2024-03-19 16:46:55 +0200

    Added parent component with event handling to render the button and the menu if clicked

---
+++ src/app/components/UI/Dropdown.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect, useRef } from "react";
+import "./dropdown.css";
+import Dropdownmenu from "./dropdownmenu";
+import DropdownItem from "./DropdownItem";
+
+function Dropdown() {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const dropdownRef = useRef(null); // Ref for the dropdown container
+
+  const toggleMenuVisibility = () => {
+    setIsMenuVisible(prevIsMenuVisible => !prevIsMenuVisible);
+  };
+
+  const closeMenu = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsMenuVisible(false);
+    }
+  };
+
+  // Add event listener when the component mounts
+  useEffect(() => {
+    document.addEventListener('mousedown', closeMenu);
+    return () => {
+      // Clean up the event listener when the component unmounts
+      document.removeEventListener('mousedown', closeMenu);
+    };
+  }, []);
+
+  return (
+    <div ref={dropdownRef} style={{ position: 'relative' }}>
+      <DropdownItem toggleMenu={toggleMenuVisibility} />
+      {isMenuVisible && <Dropdownmenu />}
+    </div>
+  );
+}
+
+export default Dropdown;
