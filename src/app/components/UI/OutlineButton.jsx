185299015178491777b7c7b7c91aaad6c4dbeb44
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2024-03-20 03:35:06 +0200

    Added event listener and a console log to simulate pressing the button and registering it

--- src/app/components/UI/OutlineButton.jsx
+++ src/app/components/UI/OutlineButton.jsx
@@ -1,12 +1,23 @@
 import React from "react";
-import styles from "./OutlineButton.module.css"
+import styles from "./OutlineButton.module.css";
 
-function OutlineButton({children}) {
-    return (
-      <div> 
-        <button role="button" tabIndex="0" className={`${styles.buttonBorder} ${styles.buttonText} ${styles.buttonColor}`}>{children}</button>
-      </div>
-    );
-  }
+function OutlineButton({ children }) {
+  const handleClick = () => {
+    console.log(children , "clicked!");
+  };
 
-  export default OutlineButton;
+  return (
+    <div>
+      <button
+        role="button"
+        tabIndex="0"
+        className={`${styles. buttonBorder} ${styles.buttonText} ${styles.buttonColor}`}
+        onClick={handleClick}
+      >
+        {children}
+      </button>
+    </div>
+  );
+}
+
+export default OutlineButton;
