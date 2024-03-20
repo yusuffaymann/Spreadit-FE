94606746a3859d29e55db457e60ebb97314866ca
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2024-03-20 13:36:56 +0200

    Added functionality to upload banner

--- src/app/settings/profile/BannerArea.jsx
+++ src/app/settings/profile/BannerArea.jsx
@@ -1,22 +1,55 @@
-import React from "react";
-import styles from "./AvatarArea.module.css";
+import React, { useState, useRef } from "react";
+import styles from "./BannerArea.module.css";
 import PlusIcon from "./PlusIcon";
 import "./Profile.css";
 
-export default function AvatarArea() {
+export default function BannerArea() {
+  const [bannerImage, setBannerImage] = useState(null);
+  const inputRef = useRef(null);
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      // You can perform additional checks/validation here if needed
+      setBannerImage(file);
+    }
+  };
+
+  const bannerStyle = {
+    backgroundImage: bannerImage ? `url(${URL.createObjectURL(bannerImage)})` : "none",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    width: "100%",
+    height: "200px", // Set your desired height
+  };
 
   return (
-      <div class="profile--banner">
-        <label class="profile--images-dragarea  profile--images-border">
-          <PlusIcon />
-          <div class="profile--images-text">
-            <span>
-              Drag and Drop or Upload{" "}
-              <span class="profile--images-textbold">Banner</span> Image
-            </span>
-          </div>
-          <div class="acceptinput"></div>
-        </label>
-      </div>
+    <div className="profile--banner">
+      <label className="profile--images-dragarea profile--images-border">
+        <div
+          style={bannerStyle}
+        >
+          {bannerImage ? null : (
+            <>
+              <PlusIcon />
+              <div className="profile--images-text">
+                <span>
+                  Drag and Drop or Upload{" "}
+                  <span className="profile--images-textbold">Banner</span> Image
+                </span>
+              </div>
+            </>
+          )}
+        </div>
+        <input
+          type="file"
+          accept=".png, .jpg"
+          onChange={handleImageUpload}
+          className="acceptinput"
+          ref={inputRef}
+          style={{ display: "none" }}
+        />
+      </label>
+    </div>
   );
 }
