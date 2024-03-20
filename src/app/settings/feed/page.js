d669eb5d72e0e569641ad434ddaf166e09e1139b
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2024-03-20 22:29:50 +0200

    Added the lock ability to feed (NSFW if disabled, will block out Blur images)

--- src/app/settings/feed/page.js
+++ src/app/settings/feed/page.js
@@ -1,22 +1,57 @@
 'use client'
+import React, { useState } from "react";
 import SettingItem from "../../components/UI/SettingItem.jsx";
 import optionData from "../options.js";
+import SettingsLayout from "../SettingsLayout"
+
 
 function Feed() {
+
+    // State to track locked components
+  const [lockedComponents, setLockedComponents] = useState({});
+
+  // Callback function to lock/unlock component
+  const handleLockComponent = (id, isLocked) => {
+    // Update locked components state based on ID and lock status
+    setLockedComponents((prevLockedComponents) => ({
+      ...prevLockedComponents,
+      [id]: isLocked,
+    }));
+  };
+
+    const handleItemClick = (id , status) => {
+        console.log(`SettingItem with ID ${id} clicked. ${status}`);
+        if (id === 1 && status === false)
+        {
+            handleLockComponent(2 , true)
+            console.log('locked');
+        }
+        else if (id ===1 && status === true)
+        {
+            handleLockComponent(2 , false)
+            console.log('unlocked');
+        }
+      };
+      
     return (
+        <>
+        <SettingsLayout />
     <div className="settings--container">
     <div className="settings--content">
           <h2 className="settings--h2">Feed settings</h2>
           <h3 className="uppercase-h3-description">Content Preferences</h3>
+
           {optionData.map(option => (
-                    option.id < 12 && <SettingItem key={option.id} option={option} />
+                    option.id < 12 && <SettingItem key={option.id} option={option} onItemClick={handleItemClick} 
+                    isLocked={lockedComponents[option.id]}/>
                 ))}
         <h3 className="uppercase-h3-description">Post Preferences</h3>
         {optionData.map(option => (
-                    option.id === 12 && <SettingItem key={option.id} option={option} />
+                    option.id === 12 && <SettingItem key={option.id} option={option} onItemClick={handleItemClick}/>
                 ))}
     </div>
     </div>
+    </>
     )
 };
 
