e013e7957e1584aa3c2a5854a1ce599eb38b229f
MahmoudLoutfy <129128879+MahmoudLoutfy@users.noreply.github.com>
2024-03-20 19:33:03 +0200

    Fixed routes

--- src/app/components/UI/Bar.jsx
+++ src/app/components/UI/Bar.jsx
@@ -7,28 +7,28 @@ export default function Bar({ selected }) {
 
     return (
         <div className={styles.tabs}> 
-            <Link className={styles.link} href="/account">
+            <Link className={styles.link} href="/settings/account">
             <h1 className={selected === 0 ? styles.selected : ""}>Account</h1> 
             </Link>
-            <Link className={styles.link} href="/profile">          
+            <Link className={styles.link} href="/settings/profile">          
             <h1 className={selected === 1 ? styles.selected : ""}>Profile</h1> 
             </Link>
-            <Link className={styles.link} href="/privacy">
+            <Link className={styles.link} href="/settings/privacy">
             <h1 className={selected === 2 ? styles.selected : ""}>Safety & Privacy</h1>
             </Link>
-            <Link className={styles.link} href="/feed">
+            <Link className={styles.link} href="/settings/feed">
             <h1 className={selected === 3 ? styles.selected : ""}>Feed settings</h1>
             </Link>
-            <Link className={styles.link} href="/notifications">
+            <Link className={styles.link} href="/settings/notifications">
                 <h1 className={selected === 4 ? styles.selected : ""}>Notifications</h1> 
             </Link>
-            <Link className={styles.link} href="/emails">
+            <Link className={styles.link} href="/settings/emails">
                 <h1 className={selected === 5 ? styles.selected : "" }>Emails</h1>
             </Link>
-            <Link className={styles.link} href="/subscriptions">
+            <Link className={styles.link} href="/settings/premium">
             <h1 className={selected === 6 ? styles.selected : ""}>Subscriptions</h1>
             </Link>
-            <Link className={styles.link} href="/messaging">
+            <Link className={styles.link} href="/settings/messaging">
             <h1 className={selected === 7 ? styles.selected : ""}>Chat & Messaging</h1>
             </Link>
         </div>
