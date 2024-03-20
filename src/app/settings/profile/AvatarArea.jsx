0e6af99b19baf4c4d473fd54e3be8131c326d367
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2024-03-20 13:36:05 +0200

    Added functionality to upload profile picture

--- src/app/settings/profile/AvatarArea.jsx
+++ src/app/settings/profile/AvatarArea.jsx
@@ -1,22 +1,44 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import styles from "./AvatarArea.module.css";
 import PlusIcon from "./PlusIcon";
 import "./Profile.css";
 
 export default function AvatarArea() {
+  const [avatarImage, setAvatarImage] = useState(null);
+  const inputRef = useRef(null);
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setAvatarImage(file);
+    }
+  };
 
   return (
-      <div className="profile--avatar">
-        <label className="profile--images-dragarea  profile--images-border">
-          <PlusIcon />
-          <div className="profile--images-text">
-            <span>
-              Drag and Drop or Upload{" "}
-              <span className="profile--images-textbold">Avatar</span> Image
-            </span>
-          </div>
-          <div className="acceptinput"></div>
-        </label>
-      </div>
+    <div className="profile--avatar">
+      <label className="profile--images-dragarea profile--images-border">
+        {avatarImage ? (
+          <img className={`${styles.box} ${styles.color} ${styles.border} }`} src={URL.createObjectURL(avatarImage)} alt="Uploaded Avatar" />
+        ) : (
+          <>
+            <PlusIcon />
+            <div className="profile--images-text">
+              <span>
+                Drag and Drop or Upload{" "}
+                <span className="profile--images-textbold">Avatar</span> Image
+              </span>
+            </div>
+          </>
+        )}
+        <input
+          type="file"
+          accept=".png, .jpg"
+          onChange={handleImageUpload}
+          className="acceptinput"
+          ref={inputRef}
+          style={{ display: "none" }}
+        />
+      </label>
+    </div>
   );
 }
