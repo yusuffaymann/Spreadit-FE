1c6f09b5b562edc9ae74a44f009871b1e3c60362
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2024-03-22 01:55:32 +0200

    The GrayButton can now be disabled by passing a prop isDisabled into it

--- src/app/settings/profile/components/GrayButton.jsx
+++ src/app/settings/profile/components/GrayButton.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import styles from "./GrayButton.module.css";
 
-function GrayButton({ children , wasClicked, svgDisplayed = '&#43;'}) {
+function GrayButton({ children , wasClicked, isDisabled}) {
 
-  return (
-    <li class={styles.buttonround} onClick={wasClicked} tabIndex="0" role="button"> 
-    <i className="icon">&#43;</i>{children}</li>
+  return (<>
+    {isDisabled && (<li class={styles.buttonroundd}  tabIndex="0" role="button"> 
+    <i className="icon">&#43;</i>{children}</li>)}
+
+    {!isDisabled && (<li class={styles.buttonround} onClick={wasClicked} tabIndex="0" role="button"> 
+    <i className="icon">&#43;</i>{children}</li>)
+  }
+    </>
   );
 }
 
