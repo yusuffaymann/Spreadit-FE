86c1cf03f01600f30ec34383a4ae37334503f7d4
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2024-03-22 01:56:58 +0200

    Minor fix to styling

--- src/app/settings/profile/Profile_images.jsx
+++ src/app/settings/profile/Profile_images.jsx
@@ -9,8 +9,8 @@ export default function ProfileImages() {
     <div>
       <div className="settings--flex">
         <div className="settings--flexheader">
-          <h3>Avatar and banner image</h3>
-          <p>Images must be .png or .jpg format</p>
+          <h3 className="settings--h3">Avatar and banner image</h3>
+          <p className="settings--p ">Images must be .png or .jpg format</p>
         </div>
         <div class="settings--flexoption">
           <div className="profile--images-flexdrag">
