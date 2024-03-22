8c4ef237f1b9e0adc331b852eeedea31015f675c
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2024-03-22 02:03:08 +0200

    Added ability for Profile_social to display selected social links as well as removing them if needed.

--- src/app/settings/profile/Profile_social.jsx
+++ src/app/settings/profile/Profile_social.jsx
@@ -1,22 +1,30 @@
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Profile.css";
 import GrayButton from "./components/GrayButton";
 import SocialLink from "./components/SocialLink";
 import GrayOutMenuWrapper from "./components/GrayOutMenu"; // Import the correct component
+import social from "../social.js";
 
-export default function ProfileSocial({ isOpen, onClose , onSelectSocial}) {
+export default function ProfileSocial({ isOpen, onClose, onSelectSocial }) {
   const [socialLinks, setSocialLinks] = useState([]);
   const [counter, setCounter] = useState(0);
 
-  const addSocialLink = (name) => {
+  const addSocialLink = (id, name, link, logo) => {
     if (counter < 5) {
       // Spread the existing socialLinks array and add the new object to it
-      setSocialLinks([...socialLinks, { name }]);
+      setSocialLinks([...socialLinks, { id, name, link, logo }]);
       setCounter(counter + 1);
     }
   };
-  
+
+  const deleteSocialLink = (id) => {
+    // Filter out the social link with the given id
+    const updatedSocialLinks = socialLinks.filter(link => link.id !== id);
+    // Update the state with the new array
+    setSocialLinks(updatedSocialLinks);
+    setCounter(counter - 1);
+  };
 
   return (
     <div>
@@ -30,14 +38,27 @@ export default function ProfileSocial({ isOpen, onClose , onSelectSocial}) {
         <div className="settings--flexoption">
           <nav className="profile--social-nav">
             <ul>
-              <GrayButton children={'Add social link'} wasClicked={onClose} />
-              <SocialLink>face</SocialLink>
               {socialLinks.map((link, index) => (
-                <SocialLink key={index}>{link.name}</SocialLink>
+                <div style={{display: "flex", alignItems: "center"}}>
+                <a href={link.link} style={{ textDecoration: "none" }}>
+                  <SocialLink key={index} logo={link.logo} name={link.name} />
+                </a>
+                </div>
               ))}
-              <GrayOutMenuWrapper isOpen={isOpen} onClose={onClose} isAddBtn = {true} onSelectWrapper={onSelectSocial}
-              addFunc={addSocialLink}/> {/* Pass onClose instead of onClick */}
+              <GrayButton children={"Add social link"} wasClicked={onClose} isDisabled={counter === 5}/>
+              <GrayOutMenuWrapper
+                isOpen={isOpen}
+                onClose={onClose}
+                onSelectWrapper={onSelectSocial}
+                addFunc={addSocialLink}
+              />{" "}
+              {/* Pass onClose instead of onClick */}
             </ul>
+            <ul>{socialLinks.map((link, index) => (
+                <div style={{display: "flex", alignItems: "center"}}>
+                  <SocialLink key={index} id={link.id} logo={link.logo} name={link.name} wasClicked={deleteSocialLink} isDeletor={true}/>
+                </div>
+              ))}</ul>
           </nav>
         </div>
       </div>
