ff2616fe77131fefaf00955f001f9692faaea41e
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2024-03-22 14:15:56 +0200

    Social Link handling was migrated to the page.js
    Also, link object's `link` was changed to `url`

--- src/app/settings/profile/Profile_social.jsx
+++ src/app/settings/profile/Profile_social.jsx
@@ -6,25 +6,7 @@ import SocialLink from "./components/SocialLink";
 import GrayOutMenuWrapper from "./components/GrayOutMenu"; // Import the correct component
 import social from "../social.js";
 
-export default function ProfileSocial({ isOpen, onClose, onSelectSocial }) {
-  const [socialLinks, setSocialLinks] = useState([]);
-  const [counter, setCounter] = useState(0);
-
-  const addSocialLink = (id, name, link, logo) => {
-    if (counter < 5) {
-      // Spread the existing socialLinks array and add the new object to it
-      setSocialLinks([...socialLinks, { id, name, link, logo }]);
-      setCounter(counter + 1);
-    }
-  };
-
-  const deleteSocialLink = (id) => {
-    // Filter out the social link with the given id
-    const updatedSocialLinks = socialLinks.filter((link) => link.id !== id);
-    // Update the state with the new array
-    setSocialLinks(updatedSocialLinks);
-    setCounter(counter - 1);
-  };
+export default function ProfileSocial({ isOpen, onClose, onSelectSocial, addSocialLink, deleteSocialLink, socialLinks, counter }) {
 
   return (
     <div>
@@ -40,8 +22,8 @@ export default function ProfileSocial({ isOpen, onClose, onSelectSocial }) {
             <ul>
               {socialLinks.map((link, index) => (
                 <div style={{ display: "flex", alignItems: "center" }}>
-                  <a href={link.link} style={{ textDecoration: "none" }}>
-                    <SocialLink key={index} logo={link.logo} name={link.name} />
+                  <a href={link.url} style={{ textDecoration: "none" }}>
+                    <SocialLink key={link.id} logo={link.logo} name={link.name} />
                   </a>
                 </div>
               ))}
