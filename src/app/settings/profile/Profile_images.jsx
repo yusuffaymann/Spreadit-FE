b72a68c86be0d2f1d31c34a0e57184fc13c1be4f
Abdullah Haytham Mohamed Salah <hedeya@gmail.com>
2024-03-27 02:14:17 +0200

    Revert "Functional documentation for ProfieImages"
    This reverts commit 471a92dbf33d6c38820d65074eaa68b2682a9954.

--- src/app/settings/profile/Profile_images.jsx
+++ src/app/settings/profile/Profile_images.jsx
@@ -4,17 +4,6 @@ import "./Profile.css";
 import AvatarArea from "./profile_images/AvatarArea";
 import BannerArea from "./profile_images/BannerArea"
 
-/**
- * Component for images section in the profile settings page. (This component and its children only have access to the setter, not the stored url)
- * @component
- * @param   {Function} setAvatarUrl         Setter for the profile image to be uploaded after upload
- * @param   {Function} setBannerUrl   Setter for the profile banner to be uploaded after upload
- * @returns {JSX.Element} The rendered ProfileImages component.
- *
- * @example
- * //Renders the images section with the setters simply logging the action made
- * <ProfileImages setAvatarUrl={console.log(`Avatar URL changed`) setBannerUrl={console.log(`Banner URL changed`)} />;
-*/
 export default function ProfileImages({setAvatarUrl, setBannerUrl}) {
 
   
