eacc235bef5446481bc9e2da005b1d2e5b53d385
Abdullah Haytham Mohamed Salah <hedeya@gmail.com>
2024-03-27 02:24:53 +0200

    Revert "Revert "Functional documentation for OutlineButton""
    This reverts commit c0be520447af576cf66881b32a5e6c43309bd4c4.

--- src/app/components/UI/OutlineButton.jsx
+++ src/app/components/UI/OutlineButton.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 import styles from "./OutlineButton.module.css";
 
+
+/**
+ * Component for rendering outline button (round white background button)
+ * @component
+ * @param {object} props  The props passed to the OutlineButton component
+ * @param {boolean} props.isDisabled  Whether the button is disabled or not
+ * @param {Function} props.btnClick The function to be called upon clicking the button
+ * @returns {JSX.Element} The rendered OutlineButton component.
+ *
+ * @example
+ * <OutlineButton isDisabled={false} btnClick={handleClick}>Click Me</OutlineButton>
+ */
 function OutlineButton(props) {
+
+  /**
+   * Leftover debug function
+   */
   const handleClick = () => {
     console.log(props.children , "clicked!");
   };
