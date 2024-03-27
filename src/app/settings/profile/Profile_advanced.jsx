4570ade1cab7c357e67f10f8b54542a0aecc4697
Abdullah Haytham Mohamed Salah <hedeya@gmail.com>
2024-03-27 02:14:13 +0200

    Revert "Functional documentation for ProfileAdvanced"
    This reverts commit 67cf06d0504497899d363ed5ca84582e481c19a7.

--- src/app/settings/profile/Profile_advanced.jsx
+++ src/app/settings/profile/Profile_advanced.jsx
@@ -3,18 +3,6 @@ import React from "react";
 import SettingItem from "../../components/UI/SettingItem.jsx"
 import optionData from "../options.js";
 
-/**
- * Component for advanced section in the profile settings page.
- * @component
- * @param   {Object} props         The props passed
- * @param   {Function} props.clickEvent   The function to be called when a switch is clicked inside the SettingItem component/container
- * @param   {Array.<boolean>} props.array     An array of API predetermined / saved true/false values for the switch components, created in Profile component
- * @returns {JSX.Element} The rendered ProfileAbout component.
- *
- * @example
- * //Renders the advanced section with a console log click event, and setting by default the switches to be on
- * <ProfileAdvanced props.isToggled={true} props.clickEvent={console.log(`Toggle state now: ${isToggled}`)} />;
-*/
 export default function ProfileAdvanced(props)
 {
     return (
