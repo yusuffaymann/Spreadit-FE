0fd7b3c7551bd34511f6eaa2c3747b27e34eb35a
Abdullah Haytham Mohamed Salah <hedeya@gmail.com>
2024-03-27 02:12:43 +0200

    Revert "Last Functional Documentation entry"
    This reverts commit 2cd76e50edb63b585da4f56d55c78fb4e909a5be.

--- src/app/components/UI/SettingItem.jsx
+++ src/app/components/UI/SettingItem.jsx
@@ -5,92 +5,23 @@ import Switch from "./Switch";
 import Dropdown from "./Dropdown";
 import OutlineButton from "./OutlineButton";
 
-/**
- * Component for rendering SettingItem (a repeated `<div>` throughout the feed and profile settings pages)
- * Manages different types of settings such as switches, dropdowns, and buttons.
- * @component
- * @param {object} props  The props passed to the SettingItem component
- * @param {object} props.option  The setting option object containing title, description, type, and other details
- * @param {boolean} props.isLocked  Whether the setting item is locked or not (currently only used for `id` 2 (NSFW Blur option) )
- * @param {boolean} props.isToggled  The state of the switch (on or off)
- * @param {number} props.defaultDropdown  The default selected ID for the dropdown menu
- * @param {Function} props.onItemClick  The function to be called upon clicking the setting item
- * @param {Function} props.dropDownClick  The function to be called upon selecting an item from the dropdown
- * @returns {JSX.Element} The rendered SettingItem component.
- *
- * @example
- * // Example usage of SettingItem component for a switch
- * <SettingItem
- *   option={{
- *     id: 1,
- *     title: "Switch Setting",
- *     description: "Description of the switch setting",
- *     type: "switch",
- *     subOptions: []
- *   }}
- *   isLocked={false}
- *   isToggled={true}
- *   onItemClick={handleItemClick}
- * />
- * @example
- * // Example usage of SettingItem component for a dropdown
- * <SettingItem
- *   option={{
- *     id: 2,
- *     title: "Dropdown Setting",
- *     description: "Description of the dropdown setting",
- *     type: "dropdown",
- *     subOptions: [],
- *   }}
- *   defaultDropdown={1}
- *   onItemClick={handleItemClick}
- *   dropDownClick={handleDrop}
- * />
- * @example
- * // Example usage of SettingItem component for a button
- * <SettingItem
- *   option={{
- *     id: 3,
- *     title: "Button Setting",
- *     description: "Description of the button setting",
- *     type: "button",
- *     buttontext: "Click Me"
- *   }}
- *   onItemClick={handleItemClick}
- * />
- */
 export default function SettingItem(props) {
   const [isSwitchToggled, setIsSwitchToggled] = useState(props.isToggled);
   const [previousSwitchState, setPreviousSwitchState] = useState(props.prevBlur);
 
-  /**
-  * Redundant function intended for switch component. Now unneeded
-  */
   const handleClick = () => {
-    // Call the callback function if exists provided by the parent
+    // Call the callback function provided by the parent
     if (props.onItemClick) {
       props.onItemClick(props.option.id);
     }
   };
 
-  /**
-  * Handles the click event of the switch component.
-  * Contains debug line
-  * Toggles the switch state and triggers the parent callback with the updated state.
-  */
   const handleSwitchToggle = () => {
     setIsSwitchToggled((prevState) => !prevState);
     console.log(`Switch toggled. New state: ${!isSwitchToggled}`);
     props.onItemClick(props.option.id, !isSwitchToggled);
   };
 
-  /**
-   * Handles the selection of an item from the dropdown menu.
-   * contains debug line
-   * calls the parent callback with the selected `id`.
-   * @param   {number} id          The ID of the parent option
-   * @param   {number} selectedId  The ID of the selected sub-option
-   */
   const handleDrop = (id,selectedId) => {
     console.log(`handleDrop: ${id} ${selectedId}`)
     props.dropDownClick(id, selectedId);
