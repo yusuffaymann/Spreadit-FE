847fb928f57ef1a36f2abffb04129b5d71d03500
Abdullah Haytham Mohamed Salah <hedeya@gmail.com>
2024-03-27 02:14:34 +0200

    Revert "Functional documentation for BannerArea"
    This reverts commit 39ab4d903c4c88a22c1ae5450f7a9e2695b2cc8f.

--- src/app/settings/profile/profile_images/BannerArea.jsx
+++ src/app/settings/profile/profile_images/BannerArea.jsx
@@ -3,19 +3,6 @@ import styles from "./BannerArea.module.css";
 import PlusIcon from "./PlusIcon";
 import "../Profile.css";
 
-
-/**
- * Component rendering the banner upload area
- * @component
- * @param   {Function} setBannerUrl     The setter to the URL link
- * @returns {JSX.Element} The rendered BannerArea component.
- *
- * @example
- * //Non interactive static area
- * <BannerArea />
- * //Print the URL to be returned
- * <bannerArea setBannerUrl={console.log(`${URL.createObjectURL(bannerImage)}`)}/>
- */
 export default function BannerArea({setBannerUrl}) {
   const [bannerImage, setBannerImage] = useState(null);
   const inputRef = useRef(null);
@@ -26,15 +13,6 @@ export default function BannerArea({setBannerUrl}) {
     }
   }, [bannerImage, setBannerUrl]);
 
-   /**
- * Handles image upload event
- * @param   {object} event The event object triggered by the image upload
- * @returns {void} Nothing returned.
- *
- * @example
- * // This will set the bannerImage object, which will then have its URL derived by above useEffect
- * <input type="file" onChange={handleImageUpload} />
- */
   const handleImageUpload = (event) => {
     const file = event.target.files[0];
     if (file) {
