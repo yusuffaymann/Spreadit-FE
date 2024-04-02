3d585290d0772572ded15acb134a842c3cc0ae22
MahmoudLoutfy <129128879+MahmoudLoutfy@users.noreply.github.com>
2024-04-02 21:18:15 +0200

    created and integrated hidden post component

--- src/app/components/post/Post.jsx
+++ src/app/components/post/Post.jsx
@@ -6,6 +6,7 @@ import Header from "./PostHeader";
 import nextImage from "../../assets/right-chevron-svgrepo-com.svg"
 import previousImage from "../../assets/left-chevron-svgrepo-com.svg"
 import PostFooter from "./PostFooter";
+import HiddenPost from "./HiddenPost";
 
 /**
  * Component for displaying the post.
@@ -16,45 +17,49 @@ function Post({ title, description, subRedditName, subRedditPicture, video, imag
 
     const displayDescription = (video===undefined && images===undefined) ? true : false;
     const [imageIndex, setImageIndex] = useState(0);
+    const hidden = false; //temporary until hidden functionality is implemented 
 
     return (
         <div className={styles.post}>
             <div className={styles.body}>
-                <Header subRedditName={subRedditName} subRedditPicture={subRedditPicture} time={time} />
-                <div className={styles.title}>{title}</div>
-                {displayDescription && <div className={styles.description}>{description}</div>}
-                <div className={styles.media}>
-                    {video !== undefined &&
-                        <iframe className={styles.video} title="Posted video"
-                        allowFullScreen
-                        src={video}
-                    />}
-                    {(video === undefined && images !==undefined) &&       
-                        <div className={styles.image} >
-                            <div className={styles.backgroundImage} style={{backgroundImage: `url(${images[imageIndex]})`}}></div>
-                            <Image src={images[imageIndex]} alt="posted image " fill style={{objectFit: "contain", maxWidth: "100%"}}  />
-                            {(images.length > imageIndex+1) &&
-                                <button type="button" className={`${styles.changeImage} ${styles.nextImage}`} onClick={() => setImageIndex(imageIndex+1)}>
-                                <Image 
-                                src={nextImage}
-                                width={16}
-                                height={16} 
-                                viewBox="0 0 20 20"
-                                alt="next image" />
-                            </button>}
-                            {(imageIndex !== 0) &&                        
-                                <button type="button" className={`${styles.changeImage} ${styles.previousImage}`} onClick={() => setImageIndex(imageIndex-1)}>
-                                <Image 
-                                src={previousImage}
-                                width={16}
-                                height={16} 
-                                viewBox="0 0 20 20"
-                                alt="previous image" />
-                            </button>}
-                        </div>
-                    }
-                </div>
-                <PostFooter upvote={() => {console.log("upvote")}} downvote={() => {console.log("downvote")}} voteCount={upVotes} commentCount={comments} isMod={true} />
+                {hidden===true && <HiddenPost />}
+                {hidden ===false && <div>
+                    <Header subRedditName={subRedditName} subRedditPicture={subRedditPicture} time={time} />
+                    <div className={styles.title}>{title}</div>
+                    {displayDescription && <div className={styles.description}>{description}</div>}
+                    <div className={styles.media}>
+                        {video !== undefined &&
+                            <iframe className={styles.video} title="Posted video"
+                            allowFullScreen
+                            src={video}
+                        />}
+                        {(video === undefined && images !==undefined) &&       
+                            <div className={styles.image} >
+                                <div className={styles.backgroundImage} style={{backgroundImage: `url(${images[imageIndex]})`}}></div>
+                                <Image src={images[imageIndex]} alt="posted image " fill style={{objectFit: "contain", maxWidth: "100%"}}  />
+                                {(images.length > imageIndex+1) &&
+                                    <button type="button" className={`${styles.changeImage} ${styles.nextImage}`} onClick={() => setImageIndex(imageIndex+1)}>
+                                    <Image 
+                                    src={nextImage}
+                                    width={16}
+                                    height={16} 
+                                    viewBox="0 0 20 20"
+                                    alt="next image" />
+                                </button>}
+                                {(imageIndex !== 0) &&                        
+                                    <button type="button" className={`${styles.changeImage} ${styles.previousImage}`} onClick={() => setImageIndex(imageIndex-1)}>
+                                    <Image 
+                                    src={previousImage}
+                                    width={16}
+                                    height={16} 
+                                    viewBox="0 0 20 20"
+                                    alt="previous image" />
+                                </button>}
+                            </div>
+                        }
+                    </div>
+                    <PostFooter upvote={() => {console.log("upvote")}} downvote={() => {console.log("downvote")}} voteCount={upVotes} commentCount={comments} isMod={true} />
+                </div>}
             </div>
         </div>
     );
