12fb7187dc2959da078292584d489748d83b3030
ahmadashry2002@gmail.com <ahmadashry2002@gmail.com>
2024-04-03 23:45:13 +0200

    modified the text area buttons

--- src/app/components/UI/CommentInput.jsx
+++ src/app/components/UI/CommentInput.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { useState,useEffect } from 'react';
 import styles from "./CommentInput.module.css"
+import Image from "next/image";
+import fontsicon from "../../assets/fonts.svg"
+import imageicon from "../../assets/image.svg"
 
-const CommentInput=({onComment})=>{
+const CommentInput=({onComment,close})=>{
     const [commentBody,setCommentBody]=useState('');
 
     const handleCommentSubmit=()=>{
@@ -26,15 +29,18 @@ const CommentInput=({onComment})=>{
                 onChange={handleCommentChange} 
             />
             <div className={styles.buttonGroup}>
-                <button className={styles.addButton} onClick={handleCommentSubmit}>Comment</button>
-                <button className={styles.embeddedButton}>Button 1</button>
-                <button className={styles.embeddedButton}>Button 2</button>
-                <button className={styles.embeddedButton}>Button 3</button>
+                <div className={styles.leftbuttons}>
+                    <Image src={imageicon} alt="image icon" className={styles.icons} />
+                    <Image src={fontsicon} alt="fonts icon" className={styles.icons} />
+                </div>
+                <div className={styles.rightbuttons}>
+                    <button className={styles.cancelButton} onClick={close}>Cancel</button>
+                    <button className={styles.addButton} onClick={handleCommentSubmit}>Comment</button>  
+                </div> 
             </div>
         </div>
     );
 }
-
 export default CommentInput;
 
 /*<input value={commentBody} className={styles.commentinput} placeholder="write your comment here" onChange={handlecommentchange} />
