fe702575d1459481ed328a47e8b8e96d7735bf68
ahmadashry2002@gmail.com <ahmadashry2002@gmail.com>
2024-04-04 02:50:16 +0200

    added is followed in comment

--- src/app/components/UI/Comment.jsx
+++ src/app/components/UI/Comment.jsx
@@ -4,13 +4,15 @@ import Image from "next/image";
 import CommentInput from "./CommentInput";
 import plusicon from "../../assets/plus-circle.svg";
 import dashicon from "../../assets/dash-circle.svg";
+import PostHeader from "../post/PostHeader";
 import styles from "./Comment.module.css"
 
 const Comment=({comment})=>{
     const [isReplying,setIsReplying]=useState(false);
     const [showReply,setShowReply]=useState(true);
     const [replies,setReplies]=useState(comment.replies);
-    const [hidden,setHidden]=useState(comment.hidden)
+    const [hidden,setHidden]=useState(comment.hidden);
+    const [followed,setFollowed]=useState(comment.userObject.followed);
     const onComment=(newReply)=>{
         setReplies((prev)=>[...prev,newReply])
         setIsReplying(false);
@@ -19,13 +21,14 @@ const Comment=({comment})=>{
        <div className={styles.commentcontainer}>
             {!hidden&&(
                     <div className={styles.commentbody}>
-                        <div>
+                        <PostHeader isprofile={true} subRedditName={comment.userObject.userName} subRedditPicture={comment.userObject.profilePicture} time={"1 mon"} banner={""} subRedditDescription={""} cakeDate={"1/1/2012"} isfollowed={followed} onFollow={()=>{setFollowed(!followed)}}/>
+                        {/* <div>
                             <img className={styles.profilePicture} 
                                 alt="Profile Picture"
                                 src={comment.userObject.profilePicture} 
                             />
                             <span>{comment.userObject.userName}</span>
-                        </div>
+                        </div> */}
                         <span>{comment.body}</span>
                         {isReplying&&(<CommentInput onComment={onComment} close={()=>setIsReplying(!isReplying)}/>)}
                         {!isReplying &&(<button className={styles.replybutton} onClick={()=>setIsReplying(!isReplying)}>Reply</button>)}  
