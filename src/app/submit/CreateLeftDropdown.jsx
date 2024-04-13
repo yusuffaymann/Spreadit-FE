3ceec4b9de9d7e9c903b59aa761f4ba899071068
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2024-04-13 17:11:15 +0200

    Separated CreateDropdownMenu component
    Separation of CreateLeftDropdown complete

--- src/app/submit/CreateLeftDropdown.jsx
+++ src/app/submit/CreateLeftDropdown.jsx
@@ -1,48 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
-import Community from "./DropdownCommunity";
+import CreateDropdownMenu from "./CreateDropdownMenu";
 import styles from "./CreateLeftDropdown.module.css";
 import "./Create.css";
-
-function Menu() {
-  return (
-    <div className={`${styles.createDropdownMenu} ${styles.DropdownMenu}`}>
-      <div className={`${styles.DropdownMenuSection}`}>
-        <div className={`${styles.userHeader}`}>Your profile</div>
-        <div className={`${styles.menuCommunityContainer}`}>
-          <div className={`${styles.menuCommunity}`}>
-            <div className={`${styles.userIconPosition}`}>
-              <img
-                alt="User avatar"
-                className={`${styles.userIconRadius} ${styles.userIconIdent}
-            ${styles.userIconBorder} ${styles.userIconSize}`}
-                src="https://styles.redditmedia.com/t5_7r9ed6/styles/profileIcon_ljpm97v13fpc1.jpg?width=256&amp;height=256&amp;crop=256:256,smart&amp;s=7ea7274e9112af1a5bf84b2549d997c4c627f72b"
-              />
-            </div>
-            <div className={`${styles.userNameFlex}`}>
-              <span className={`${styles.userNameText}`}>u/Testing</span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className={`${styles.DropdownMenuSection}`}>
-        <div className={`${styles.DropdownMenuCommunityHeader}`}>
-          <span className={`${styles.DropdownMenuCommunityHeaderText}`}>
-            Your communities
-          </span>
-          <button
-            role="button"
-            tabIndex="0"
-            className={`${styles.buttonCreateNew} ${styles.buttonCreateNewMargin}
-            ${styles.buttonCreateNewText} ${styles.buttonCreateNewAlignment}`}
-          >
-            Create New
-          </button>
-        </div>
-        <Community /><Community /><Community /><Community /><Community /><Community /><Community /><Community /><Community /><Community />
-      </div>
-    </div>
-  );
-}
 
 export default function CreateLeftDropdown({ current, setter }) {
   const [menuVisible, setMenuVisible] = useState(false);
@@ -104,7 +63,7 @@ export default function CreateLeftDropdown({ current, setter }) {
             </svg>
           </span>
         </div>
-        {menuVisible && <Menu />}
+        {menuVisible && <CreateDropdownMenu />}
       </div>
     </div>
   );
